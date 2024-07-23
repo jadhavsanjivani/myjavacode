11e8e4cd7db5e93a4d3dcb39f1367bace8e53f19
Sanjivani Rathod <sanjivanijadhav228@gmail.com>
2024-07-23 22:50:28 +0530

    Assignment 54

---
+++ assignment/Assignment54_UtilizeStack_Methods.java
@@ -0,0 +1,23 @@
+package assignment;
+
+import java.util.Stack;
+
+public class Assignment54_UtilizeStack_Methods 
+{
+
+	public static void main(String[] args)
+	{
+		Stack s = new Stack();
+		s.add("Ram");
+		s.add("Sita");
+		s.push("Urvish");
+		System.out.println(s);
+		System.out.println(s.peek());
+		System.out.println(s);
+		System.out.println(s.pop());
+		System.out.println(s);
+		
+
+	}
+
+}
